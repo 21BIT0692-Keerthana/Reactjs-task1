338ad56335ffeac74732c8b1df0a9820a96918a0
21BIT0692-Keerthana <26122003keerthana@gmail.com>
2023-10-14 21:02:04 +0530

    first commit

---
+++ src/WordCounter.js
@@ -0,0 +1,39 @@
+// WordLCounter.js 
+import React, { useState } from "react"; 
+import "./WordCounter.css"; 
+
+function WordCounter() { 
+	const [text, setText] = 
+		useState(""); 
+	const wordCount = text 
+		.split(/\s+/) 
+		.filter(Boolean).length; 
+	
+
+	const handleTextChange = (e) => { 
+		setText(e.target.value); 
+	}; 
+
+	return ( 
+		<div> 
+			<textarea 
+				placeholder= 
+				"Type your text here..."
+				onChange={ 
+					handleTextChange 
+				} 
+				value={text} 
+				rows={5} 
+				cols={50} 
+			/> 
+			<p> 
+				Word Count: 
+				{wordCount} 
+			</p> 
+			
+			
+		</div> 
+	); 
+} 
+
+export default WordCounter;
